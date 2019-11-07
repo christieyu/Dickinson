d2a8feb2f007e6c5ae8f0c4962bb7757fd636a60
Christie Yu <43098796+christieyu@users.noreply.github.com>
2019-11-07 00:35:18 -0500

    Create extras.py

---
+++ extras.py
@@ -0,0 +1,121 @@
+extras = [
+  {
+    'title': 'A brief but patient illness',
+    'poems': [u'A brief, but patient illness \u2014\nAn hour to prepare \u2014\nAnd one below, this morning\nIs where all the angels are \u2014\nIt was a short procession \u2014\nThe Bobolink was there \u2014\nAn aged Bee addressed us \u2014\nAnd then we knelt in prayer \u2014\nWe trust that she was willing \u2014\nWe ask that we may be \u2014\nSummer \u2014 Sister \u2014 Seraph!\nLet us go with thee!']
+  },
+
+  {	
+    'title': 'A chastened Grace is twice a Grace',
+    'poems': [u'A chastened Grace is twice a Grace \u2014\nNay, \'tis a Holiness.']
+  },
+
+  {	
+    'title': 'But that defeated accent',
+    'poems': [u'But that defeated accent\nis louder now than him\nEternity may imitate\nThe Affluence of time', u'But that suspended syllable\nIs wealthier than him\nEternity may imitate\nThe Affluence of time', u'But that arrested syllable\nIs wealthier than him\nEternity may imitate\nThe Affluence of time', u'But Love\'s dispelled Emolument\nFinds no Abode in him \u2014\nEternity may imitate\nThe Affluence of time', u'But Love\'s dispelled Emolument\nHas no Abode in him \u2014\nEternity may imitate\nThe Affluence of time', u'But Love\'s dispelled Emolument\nHas no retrieve in him\nEternity may imitate\nThe Affluence of time']
+  },
+
+  {	
+    'title': 'Ferocious as a Bee without a wing',
+    'poems': [u'Ferocious as a Bee without a wing\nThe Prince of Honey and the Prince of Sting\nSo plain a flower presents her Disk to thee']
+  },
+
+  {	
+    'title': 'Flees so the phantom meadow',
+    'poems': [u'Flees so the phantom meadow\nBefore the breathless Bee \u2014\nSo bubble brooks in deserts\nOn ears that dying lie \u2014\nBurn so the evening spires\nTo eyes that Closing go \u2014\nHangs so distant Heaven\nTo a hand below.']
+  },
+
+  {	
+    'title': 'Fly \u2014 fly \u2014 but as you fly',
+    'poems': [u'Fly \u2014 fly \u2014 but as you fly \u2014\nRemember \u2014 the second pass you by \u2014\nThe Second is pursuing the Century\nThe Century is chasing Eternity \u2014\nAh the Responsibility \u2014\nNo wonder that the little Second flee \u2014\nOut of its frightened way \u2014', u'Fly \u2014 fly \u2014 but as you fly \u2014\nRemember \u2014 the second pass you by \u2014\nThe Second is pursuing the Century\nThe Century is chasing Eternity \u2014\nwhat a Responsibility \u2014\nNo wonder that the little Second flee \u2014\nOut of its frightened way \u2014']
+  },
+
+  {	
+    'title': 'I do not care - why should I care',
+    'poems': [u'I do not care \u2014 why should I care\nAnd yet I fear I\'m caring\nTo rock a fretting truth to sleep \u2014\nIs short security\nThe terror it will wake\npersistent as perdition\nIs harder to face\nthe frank adversity \u2014\nThere is an awful yes in every constitution', u'I do not care \u2014 why should I care\nAnd yet I fear I\'m caring\nTo rock a crying truth to sleep \u2014\nIs short security\nThe terror it will wake\npersistent as perdition\nIs harder to face\nthe frank adversity \u2014\nThere is an awful yes in every constitution', u'I do not care \u2014 why should I care\nAnd yet I fear I\'m caring\nTo rock a wailing truth to sleep \u2014\nIs short security\nThe terror it will wake\npersistent as perdition\nIs harder to face\nthe frank adversity \u2014\nThere is an awful yes in every constitution', u'I do not care \u2014 why should I care\nAnd yet I fear I\'m caring\nTo rock a fretting truth to sleep \u2014\nIs no security\nThe terror it will wake\npersistent as perdition\nIs harder to face\nthe frank adversity \u2014\nThere is an awful yes in every constitution', u'I do not care \u2014 why should I care\nAnd yet I fear I\'m caring\nTo rock a fretting truth to sleep \u2014\nIs frail security\nThe terror it will wake\npersistent as perdition\nIs harder to face\nthe frank adversity \u2014\nThere is an awful yes in every constitution', u'I do not care \u2014 why should I care\nAnd yet I fear I\'m caring\nTo rock a fretting truth to sleep \u2014\nIs poor security\nThe terror it will wake\npersistent as perdition\nIs harder to face\nthe frank adversity \u2014\nThere is an awful yes in every constitution']
+  },
+
+  {	
+    'title': 'I held it so tight that I lost it',
+    'poems': [u'I held it so tight that I lost it\nSaid the Child of the Butterfly\nOf many a vaster Capture\nThat is the Elegy \u2014', u'I held it so tight that I lost it\nSaid the Boy of the Butterfly\nOf many a vaster Capture\nThat is the Elegy \u2014']
+  },
+
+  {	
+    'title': 'If I should see a single bird',
+    'poems': [u'If I should see a single bird']
+  },
+
+  {	
+    'title': 'In the name of the bee',
+    'poems': [u'In the name of the bee \u2014\nAnd of the Butterfly \u2014\nAnd of the Breeze \u2014\nAmen!']
+  },
+
+  {	
+    'title': 'Incredible the Lodging',
+    'poems': [u'Incredible the Lodging\nBut limited the Guest']
+  },
+
+  {	
+    'title': 'It is the Meek that Valor wear',
+    'poems': [u'It is the Meek that Valor wear\nToo mighty for the Bold.']
+  },
+
+  {	
+    'title': 'Let others \u2014 show this Surry\'s Grace',
+    'poems': [u'Let others \u2014 show this Surry\'s Grace \u2014\nMyself \u2014 assist his Cross \u2014']
+  },
+
+  {	
+    'title': 'Life is death we\'re lengthy at',
+    'poems': [u'Life is death we\'re lengthy at,\nDeath the hinge to life.']
+  },
+
+  {	
+    'title': 'No Rose, yet felt myself a\'bloom',
+    'poems': [u'No Rose, yet felt myself a\'bloom,\nNo Bird \u2014 yet rode in Ether \u2014']
+  },
+
+  {	
+    'title': 'One note from One Bird',
+    'poems': [u'One note from One Bird\nIs better than a Million Word \u2014\nA scabbard has \u2014 but one sword']
+  },
+
+  {	
+    'title': 'Show me Eternity, and I will show you Memory',
+    'poems': [u'Show me Eternity, and I will show you Memory \u2014\nBoth in one package lain\nAnd lifted back again \u2014\n\nBe Sue, while I am Emily \u2014\nBe next, what you have ever been, Infinity \u2014']
+  },
+
+  {	
+    'title': 'The Blood is more showy than the Breath',
+    'poems': [u'The Blood is more showy than the Breath\nBut it cannot dance as well \u2014', u'The Blood is more gaudy than the Breath\nBut it cannot dance as well \u2014']
+  },
+
+  {	
+    'title': 'The lovely flowers embarrass me',
+    'poems': [u'The lovely flowers embarrass me,\n They make me regret I am not a Bee \u2014']
+  },
+
+  {	
+    'title': 'To him who keeps an orchis\' heart',
+    'poems': [u'To him who keeps an orchis\' heart \u2014\nThe swamps are pink with June.']
+  },
+
+  {	
+    'title': 'To lose if one can find again',
+    'poems': [u'To lose \u2014 if One can find again \u2014\nTo miss \u2014 if One shall meet \u2014\nThe Burglar cannot rob \u2014 then \u2014\n The Broker cannot cheat.\nSo build the hillocks gaily \u2014\nThou little spade of mine\nLeaving nooks for Daisy\nAnd for Columbine \u2014\nYou and I the secret\nOf the Crocus know \u2014\nLet us chant it softly \u2014\n"There is no more snow"!']
+  },
+
+  {	
+    'title': 'When Continents expire',
+    'poems': [u'When Continents expire\nThe Giants they discarded \u2014 are\nPromoted to enture \u2014']
+  },
+
+  {	
+    'title': 'Where I am not afraid to go',
+    'poems': [u'Where I am not afraid to go\nI may confide my Flower \u2014\nWho was not Enemy of Me\nWill gentle be, to Her \u2014\n\nNor separate, Herself and Me\nBy Distances become \u2014\nA single Bloom we constitute\nDeparted, or at Home \u2014']
+  }
+]
+
+# for i in extras:
+#     thisdict = (i['poems'])
+#     for j in thisdict:
+#         print(j + '\n')
