e5d23f6ab2976c4d274a7d7fabb152203e7b9a73
Greta Browne <greta.browne@yale.edu>
2019-11-12 13:23:20 -0500

    app.py setup

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, MongoClient
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ScarlettExperiment"
+app.config["MONGO_URI"] = "mongodb+srv://scarlettexperiment-1pxrx.mongodb.net/test"
 mongo = PyMongo(app)
+app.config["CLIENT"] = MongoClient("mongodb+srv://scarlettexperiment-1pxrx.mongodb.net/test", username="gretabrowne", password="henry811", authSource="admin")
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	return get("http://localhost:3000")
+
+	# return get(f"localhost vue server)
+
+@app.route("/poems")
+def poems():
+	client = app.config["CLIENT"]
+	print(client)
+	db = client.DickinsonDB
+	col = db.DickinsonCOLL
+	poems = list(col.find())
+	return poems
