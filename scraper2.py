f7a9fd527d5adfeda417eb94763f6dc0daa7974c
Christie Yu <43098796+christieyu@users.noreply.github.com>
2019-12-09 22:48:01 -0500

    Create scraper2.py

---
+++ scraper2.py
@@ -0,0 +1,55 @@
+# Christie Yu, Oct 31 2019
+
+from bs4 import BeautifulSoup
+import requests
+# boilerplate from https://codeburst.io/web-scraping-101-with-python-beautiful-soup-bb617be1f486
+
+
+page_link = 'https://www.bartleby.com/113/indexlines.html'
+page_response = requests.get(page_link, timeout=5)
+soup1 = BeautifulSoup(page_response.content, "html.parser")
+
+print(soup1)
+
+textContent = []
+output = []
+
+# table = soup1.find_all('table')[2]
+# fields = ["title", "text"]
+# print(table)
+
+# for tr in table.find_all('tr'):
+#     tds = tr.find_all('td')
+#     if len(tds) == 7:
+#         data = {}
+#         for i in range(6):
+#             data[fields[i]] = tds[i].text.strip()
+
+#         ahref = tr.find('a')
+#         if ahref != None:
+#             poem_link = ahref['href']
+#             poem_response = requests.get(poem_link, timeout=5)
+#             soup2 = BeautifulSoup(poem_response.content, "html.parser")
+
+#             poemdivs = soup2.find_all('div', {'class':'poem'})
+
+#             poem_list = []
+
+#             for poem in poemdivs:
+#                 poem_version = poem.find('p').text.strip()
+#                 poem_list.append(poem_version)
+
+#             data["poems"] = poem_list
+
+#         else:
+#             data["poems"] = None
+
+#         output.append(data)
+#         print(data)
+
+
+# print(output)
+
+# # You can access a list of dicts (one dict per row entry) with "output"
+# # The field names are: "title", "fasc", "pubdate", "first_ID", "collect_ID", "J_ID", "F_ID", "poems"
+# # "poems" is a list of poem versions given by Wikipedia
